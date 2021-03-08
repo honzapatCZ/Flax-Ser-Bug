5bf4cf6873200208d500151c4d7edf2d9a573d77
honzapatCZ <honzapatCZ@email.cz>
2021-03-08 10:53:00 +0100

    the bug

---
+++ Source/Game/Child.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using FlaxEngine;
+
+namespace Game
+{
+    public class Child : Parent
+    {
+    }
+}
---
+++ Source/Game/Parent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using FlaxEngine;
+
+namespace Game
+{
+    public class Parent : Script
+    {
+        [Serialize]
+        Actor theActor;
+        [NoSerialize]
+        public Actor TheActor
+        {
+            get => theActor;
+            set => theActor = value;
+        }
+    }
+}
